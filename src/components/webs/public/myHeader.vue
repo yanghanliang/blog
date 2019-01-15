07495e592db462f9f85ac084c77bb0fd91bc399d
yanghanliang <1218866243@qq.com>
2019-01-15 22:02:36 +0800

    修改顶部导航组件(blog已初步完成)

--- src/components/webs/public/myHeader.vue
+++ src/components/webs/public/myHeader.vue
@@ -1,41 +1,52 @@
 <template>
-    <header class="clearfix">
-        <div class="header_left">logo(首页)</div>
-        <ul class="header_right">
-            <li>
-                <router-link to="/catalogList">目录列表</router-link>
-            </li>
-            <li>
-            <a href="#">关于我</a>
-            </li>
-            <li>
-            <a href="#">学习笔记</a>
-            </li>
-            <li>
-            <a href="#">资源共享</a>
-            </li>
-        </ul>
-    </header>
+    <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <!-- <a href="#">logo</a> -->
+        <el-menu-item index="1">目录</el-menu-item>
+        <el-submenu index="2">
+            <template slot="title">我的工作台</template>
+            <el-menu-item index="2-1">选项1</el-menu-item>
+            <el-menu-item index="2-2">选项2</el-menu-item>
+            <el-menu-item index="2-3">选项3</el-menu-item>
+            <el-submenu index="2-4">
+                <template slot="title">选项4</template>
+                <el-menu-item index="2-4-1">选项1</el-menu-item>
+                <el-menu-item index="2-4-2">选项2</el-menu-item>
+                <el-menu-item index="2-4-3">选项3</el-menu-item>
+            </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+    </el-menu>
 </template>
 
 <script>
 export default {
-  name: 'myHeard'
+  name: 'myHeard',
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
 <style scoped>
 /* header_right-start */
-.header_right>li {
-    display: inline-block;
-}
-
-.header_right>li {
-    margin-right: 0.16rem;
-}
-
-.header_right>li:nth-last-child(1) {
-    margin-right: 1rem;
+.el-menu-demo>li {
+    float: right;
 }
 /* header_right-end */
 </style>
