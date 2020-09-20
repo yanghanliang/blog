f37983e217546e4e188e64c3d485f84171304b1d
yanghanliang <1218866243@qq.com>
2020-09-20 16:50:32 +0800

    fix:--other=文章分类逻辑修改

--- src/components/webs/admin/category/editCategory.vue
+++ src/components/webs/admin/category/editCategory.vue
@@ -4,12 +4,6 @@
             <el-form-item label="分类名称">
                 <el-input v-model="form.classname"></el-input>
             </el-form-item>
-			<el-form-item label="类型">
-                <el-select v-model="form.type" multiple placeholder="请选择分类类型">
-                    <el-option label="文章" value="1">文章</el-option>
-                    <el-option label="书签" value="2">书签</el-option>
-                </el-select>
-            </el-form-item>
             <el-form-item label="层级">
                 <el-select v-model="form.layername" placeholder="请选择" @change="changeSelectValue">
                     <el-option v-for="item in categoryData" :key="item.id" :label="item.classname" :value="item.id">
@@ -34,7 +28,6 @@ export default {
 				id: this.$route.params.categoryId,
 				classname: '',
 				pid: '',
-				type: [],
 				layername: '' // 层名
 			}
 		}
@@ -46,29 +39,16 @@ export default {
 	methods: {
 		async getCategoryData () {
 			const data = await this.$http.post('category') // 获取分类数据
-			this.categoryData = data.list // 将分类数据赋值给 vue
+			this.categoryData = [].concat({ id: 0, pid: 0, classname: '第一层级' }, data.list) // 将分类数据赋值给 vue
 		},
 		async getEditCategoryData () { // 获取需要修改的分类数据
 			const data = await this.$http.get(`editCategory/${this.form.id}`)
 			this.form.classname = data[0].classname
 			this.form.pid = data[0].pid
-			this.form.layername = data[0].layername
-			this.form.type = data[0].type.split(',')
-			const categoryData = this.categoryData
-			for (var i = 0; i < categoryData.length; i++) {
-				if (data[0].pid === 0) { // 判断是否是第一层级
-					this.form.layername = '第一层'
-					return false
-				}
-				if (categoryData[i].id === data[0].pid) { // 找到对应层级
-					this.form.layername = categoryData[i].classname
-				}
-			}
+			this.form.layername = data[0].pid
 		},
 		async editCategory () {
-			const {
-				data
-			} = await this.$http.put('updateCategory', this.form)
+			const data = await this.$http.put('updateCategory', this.form)
 			if (data.status === 200) {
 				this.$message({
 					message: data.msg,
@@ -77,9 +57,6 @@ export default {
 				// 跳转页面
 				this.$router.push({
 					name: 'categoryList',
-					query: {
-						type: this.form.type[0]
-					}
 				})
 			} else {
 				this.$message({
